9775219e283ca0e8b06b7add7fa4d5b8670c57e8
NuzGooL <ardeniaanderland@gmail.com>
2023-11-11 06:37:29 +0300

    Initial commit

---
+++ src/main/java/staffGroup/Homework2_13/StaffBookApplication.java
@@ -0,0 +1,13 @@
+package staffGroup.Homework2_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/staffGroup/Homework2_13/StaffBookApplicationTests.java
@@ -0,0 +1,13 @@
+package staffGroup.Homework2_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
